8ebfbb122d3ccb85831b3ec9fcfdf20adb79de78
hua wei  ding <weil_friend@hotmail.com>
2022-04-19 10:00:27 +0800

    2.0

--- basic/consumer/src/main/java/com/huaweicloud/samples/ConsumerController.java
+++ basic/consumer/src/main/java/com/huaweicloud/samples/ConsumerController.java
@@ -1,6 +1,7 @@
 package com.huaweicloud.samples;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,10 +15,18 @@ public class ConsumerController {
   @Autowired
   private FeignConsumerService feignConsumerService;
 
+  @Value("${consumer.config}")
+  private String configValue;
+
   // consumer service which delegate the implementation to provider service.
   @GetMapping("/sayHello")
   public String sayHello(@RequestParam("name") String name) {
     return restTemplate.getForObject("http://basic-provider/sayHello?name={1}", String.class, name);
+  }
+
+  @GetMapping("/getConfig")
+  public String getConfig() {
+    return restTemplate.getForObject("http://basic-provider/sayHello?name={1}", String.class, configValue);
   }
 
   @GetMapping("/sayHelloFeign")
