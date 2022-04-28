603b4fe74e3bc055155bb1db7edab89a96641151
hua wei  ding <weil_friend@hotmail.com>
2022-04-28 08:33:56 +0800

    验证feign中URL的动态配置

--- basic/consumer/src/main/java/com/huaweicloud/samples/FeignConsumerService.java
+++ basic/consumer/src/main/java/com/huaweicloud/samples/FeignConsumerService.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "basic-provider")
+@FeignClient(value = "feignConsumerService",url = "${provider.url}")
 public interface FeignConsumerService {
 
     @GetMapping("/sayHelloFeign")
